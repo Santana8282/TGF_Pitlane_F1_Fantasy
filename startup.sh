dd6e8f9287514ba2dc058cd7c814db5a63833788
Daniel Santana <danielsantanahernandez22@gmail.com>
2026-05-04 23:36:42 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,12 +8,12 @@ echo "server {
         try_files \$uri \$uri/ /index.php\$is_args\$args;
     }
 
-    location ~ \.php$ {
+    location ~ \.php\$ {
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_index index.php;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
     }
-}" > /etc/nginx/sites-available/default
+}" > /home/site/nginxconf/default.conf
 
-service nginx restart
+service nginx reload 2>/dev/null || true
